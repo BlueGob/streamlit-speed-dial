78aec6b7d5a7f4d51da9ec91a12c2aa471e16a8d
BlueGob <moslemgannoun@gmail.com>
2024-01-10 16:59:19 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-speed-dial",
+    version="0.0.1",
+    author="Moslem gannoun",
+    author_email="moslemgannoun@gmail.com",
+    description="Streamlit component that allows you to make speed dial ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlueGob/streamlit-speed-dial",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)
